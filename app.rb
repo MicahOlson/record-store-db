c771663256d9488c326305ea94b9dafab7a96093
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-10 08:30:29 -0800

    add return to home links

--- app.rb
+++ app.rb
@@ -18,40 +18,40 @@ get('/albums') do
   erb(:albums)
 end
 
-get ('/albums/new') do
+get('/albums/new') do
   erb(:new_album)
 end
 
-post ('/albums') do
+post('/albums') do
   name = params[:album_name]
   album = Album.new({name: name.gsub(/'/, "''")})
   album.save
   redirect to('/albums')
 end
 
-get ('/albums/:id') do
+get('/albums/:id') do
   @album = Album.find(params[:id].to_i)
   erb(:album)
 end
 
-get ('/albums/:id/edit') do
+get('/albums/:id/edit') do
   @album = Album.find(params[:id].to_i)
   erb(:edit_album)
 end
 
-patch ('/albums/:id') do
+patch('/albums/:id') do
   @album = Album.find(params[:id].to_i)
-  @album.update(params[:name].gsub(/'/, "''"))
+  @album.update(params[:album_name].gsub(/'/, "''"))
   redirect to('/albums')
 end
 
-delete ('/albums/:id') do
+delete('/albums/:id') do
   @album = Album.find(params[:id].to_i)
   @album.delete
   redirect to('/albums')
 end
 
-get ('/albums/:id/songs/:song_id') do
+get('/albums/:id/songs/:song_id') do
   @song = Song.find(params[:song_id].to_i)
   erb(:song)
 end
@@ -63,14 +63,14 @@ post('/albums/:id/songs') do
   erb(:album)
 end
 
-patch ('/albums/:id/songs/:song_id') do
+patch('/albums/:id/songs/:song_id') do
   @album = Album.find(params[:id].to_i)
   song = Song.find(params[:song_id].to_i)
-  song.update({track: params[:track], name: params[:album_name].gsub(/'/, "''"), album_id: @album.id})
+  song.update({track: params[:song_track], name: params[:song_name].gsub(/'/, "''"), album_id: @album.id})
   erb(:album)
 end
 
-delete ('/albums/:id/songs/:song_id') do
+delete('/albums/:id/songs/:song_id') do
   song = Song.find(params[:song_id].to_i)
   song.delete
   @album = Album.find(params[:id].to_i)
@@ -90,8 +90,7 @@ post('/artists') do
   name = params[:artist_name]
   artist = Artist.new({name: name})
   artist.save
-  @artists = Artist.all
-  erb(:artists)
+  redirect to('/artists')
 end
 
 get('/artists/:id') do
@@ -99,21 +98,19 @@ get('/artists/:id') do
   erb(:artist)
 end
 
-get ('/artists/:id/edit') do
+get('/artists/:id/edit') do
   @artist = Artist.find(params[:id].to_i)
   erb(:edit_artist)
 end
 
 patch('/artists/:id') do
   @artist = Artist.find(params[:id].to_i)
-  @artist.update(params[:artist_name].gsub(/'/, "''"))
-  @artists = Artist.all
-  erb(:artists)
+  @artist.update({name: params[:artist_name].gsub(/'/, "''")})
+  redirect to('/artists')
 end
 
 delete('/artists/:id') do
   @artist = Artist.find(params[:id].to_i)
   @artist.delete
-  @artists = Artist.all
-  erb(:artists)
+  redirect to('/artists')
 end
